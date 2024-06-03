8588e82fe7c545663c9ffa2133e4ec36f641039b
leonkrumel <rehmannleon3@gmail.com>
2024-06-03 16:16:25 +0200

    no message

--- Assets/+++Workdata/_Scripts/CharakterControling/CharakterMovement.cs
+++ Assets/+++Workdata/_Scripts/CharakterControling/CharakterMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,38 +8,75 @@ public class CharakterMovement : MonoBehaviour
 {
     private int Framecounter = 0;
 
+    private Rigidbody2D rb;
+    private float inputDirection;
+   //klammer mit altgr 8/9
+    [SerializeField] private float jumpforce = 5f;
+    [SerializeField] private float movementspedd = 5f;
+
+    [SerializeField] private Transform groundcheckPosition;
+    [SerializeField] private float groundcheckRadius;
+    [SerializeField] private LayerMask layerGroundcheck;
+
+    private bool isFacingRight = true;
+
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        
         Debug.Log(message: "Start!");
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() { }
+
+    private void FixedUpdate()
     {
-        
-       
+        rb.velocity = new Vector2(x:inputDirection * movementspedd, y: rb.velocity.y);
     }
 
     void OnJump()
     {
-        Framecounter = Framecounter + 1;
-        Debug.Log(message:"Jump!"+ Framecounter);
+        if (Physics2D.OverlapCircle(groundcheckPosition.position,groundcheckRadius, layerGroundcheck))
+        {
+            rb.velocity = new Vector2(x: 0f, y: jumpforce);
+        }
     }
     void OnSneak()
     {
         Framecounter = Framecounter + 1;
         Debug.Log(message:"sneak!"+ Framecounter);
     }
-    void OnSprint()
+    void OnSprint(InputValue inputvalue)
     {
-        Framecounter = Framecounter + 1;
-        Debug.Log(message:"SPRINT!"+ Framecounter);
+        float isSprinting = inputvalue.Get<float>();
+        Debug.Log(message: "SPRINT!" + isSprinting);
     }
-    void OnMove()
+    void OnMove(InputValue inputValue)
     {
-        Framecounter = Framecounter + 1;
-        Debug.Log(message:"Move!"+ Framecounter);
+        inputDirection = inputValue.Get<float>();
+        Debug.Log(message:"Move!"+ inputDirection);
+        if (inputDirection > 0 && !isFacingRight)
+        {
+           Flip();
+           
+           
+        }else if (inputDirection <0 && isFacingRight)
+        {
+            Flip();
+            
+        }
+        
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = transform.localScale;
+        currentScale.x = currentScale.x * -1;
+        transform.localScale = currentScale;
+
+        isFacingRight = !isFacingRight;
     }
 
 }
